afc3ca7d1e77107bb609f5e31ba88297438d678a
Алексей Н. Кузнецов <smith@kb51.local>
2021-03-26 12:55:12 +0700

    Fix desktop bugs. Compress images

---
+++ mail.php
@@ -0,0 +1,34 @@
+<?php
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "Ошибка! Вы не можете получить доступ напрямую - воспользуйтесь формой";
+}
+$name = $_POST['clientName'];
+$phone = $_POST['clientPhone'];
+$visitor_email = "alegorn.mail@gmail.com";
+$message = "";
+
+//Validate first
+if(empty($name)||empty($phone))
+{
+    echo "Имя и телефон обязательны!";
+    exit;
+}
+
+
+$email_from = 'hello@blanchard.ru';
+$email_subject = "Обратный звонок на $phone, $name";
+$email_body = "$name, заказал обратный звонок на номер $phone.\n".
+                            // "Here is the message:\n $message".
+
+$to = "alegorn.mail@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thankyou.html');
+
+
+?>
